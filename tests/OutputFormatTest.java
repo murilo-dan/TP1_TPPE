c0a334646ee5801c3e3473792f27061e86fca294
Rafael Ribeiro <rafaelflarrn@gmail.com>
2021-10-19 23:41:29 -0300

    Adiciona teste de triangulacao formato de saida
    Co-authored-by: Murilo Loiola <murilo.loiola.dan@gmail.com>

--- tests/OutputFormatTest.java
+++ tests/OutputFormatTest.java
@@ -13,4 +13,9 @@ public class OutputFormatTest {
     public void getFormatColuna() {
         assertEquals(false, OutputFormat.COLUNA.getFormat());
     }
+
+    @Test
+    public void getFormatNotEquals() {
+        assertNotEquals(true, OutputFormat.COLUNA.getFormat());
+    }
 }
