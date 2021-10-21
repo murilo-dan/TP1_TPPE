a883af415599f0da638c412c682c16f6a2c227b1
Rafael Ribeiro <rafaelflarrn@gmail.com>
2021-10-20 22:36:57 -0300

    Adiciona teste de falsificacao parser
    Co-authored-by: Murilo Loiola <murilo.loiola.dan@gmail.com>

--- src/FileReader.java
+++ src/FileReader.java
@@ -2,25 +2,56 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class FileReader {
 
-    public static List<String> readFile(String fileName) throws ArquivoNaoEncontradoException {
+    private int evolutions;
+    private int analysis;
+    List<String> lines;
+    private Queue<Integer> queueEvolutions;
+    private Queue<Integer> queueAnalysis;
 
-        List<String> lines = Collections.emptyList();
-        try
-        {
+    public FileReader() {
+    }
+
+    public List<String> readFile(String fileName) throws ArquivoNaoEncontradoException {
+        queueEvolutions = new ArrayDeque<>();
+        queueAnalysis = new ArrayDeque<>();
+        lines = Collections.emptyList();
+        try {
             lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
-        }
-
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             throw new ArquivoNaoEncontradoException(fileName);
         }
+
+        for (int i = 0; i < lines.size(); i++) {
+            if(lines.get(i).contains("Evolution")) {
+                queueEvolutions.add(evolutions);
+                analysis = 0;
+                evolutions++;
+            } else {
+                analysis++;
+            }
+            if(i + 1 < lines.size() && lines.get(i + 1).contains("Evolution")) {
+                queueAnalysis.add(analysis);
+            }
+        }
+        queueAnalysis.add(analysis);
+//        for (int i = 0; i < evolutions; i++) {
+//            System.out.print(queue.poll() + " ");
+//            System.out.println(queue2.poll());
+//        }
         return lines;
 
+    }
+
+    public int getEvolutions() {
+        return 0;
+    }
+
+    public int getAnalysis() {
+        return 0;
     }
 
 }
